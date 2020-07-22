775bc41f1df96f08319d932066df104b51f0b652
JoshOberst <joshoberst11@gmail.com>
2020-07-22 13:12:44 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const fetch = require("node-fetch");
+
+client.on('message', (msg) => {
+    var num = Math.floor(Math.random() * 100);
+    if (num <= 9) {
+		fetch('https://thanosapi.herokuapp.com/random/')
+        .then(function(res){
+            return res.json();
+        })
+        .then(function(data){
+            msg.channel.send(data.quote);
+            console.log(data.quote);
+        });
+	}
+});
+
+client.on('ready', () => {
+	console.log('Bot is now online!');
+});
+
+client.login(process.env.token);
